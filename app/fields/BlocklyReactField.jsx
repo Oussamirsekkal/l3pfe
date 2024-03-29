68e28a18455fe0f21ce635f470326978e9a821be
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2024-03-29 05:31:56 +0100

    inject blockly+

--- app/fields/BlocklyReactField.jsx
+++ app/fields/BlocklyReactField.jsx
@@ -24,14 +24,11 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom/client';
 import * as Blockly from 'blockly/core';
 
-
 class BlocklyReactField extends Blockly.Field {
-
-  SERIALIZABLE = true
+  SERIALIZABLE = true;
 
   static fromJson(options) {
     // `this` might be a subclass of BlocklyReactField if that class doesn't
@@ -41,32 +38,30 @@ class BlocklyReactField extends Blockly.Field {
 
   showEditor_() {
     this.div_ = Blockly.DropDownDiv.getContentDiv();
-    ReactDOM.render(this.render(),
-      this.div_);
+    this.root_ = ReactDOM.createRoot(this.div_);
+    this.root_.render(this.render());
 
     var border = this.sourceBlock_.style.colourTertiary;
     border = border.colourBorder || border.colourLight;
     Blockly.DropDownDiv.setColour(this.sourceBlock_.getColour(), border);
-
     Blockly.DropDownDiv.showPositionedByField(
-      this, this.dropdownDispose_.bind(this));
+      this,
+      this.dropdownDispose_.bind(this)
+    );
   }
 
   dropdownDispose_() {
-    ReactDOM.unmountComponentAtNode(this.div_);
+    this.root_.unmount();
   }
 
   render() {
-    return <FieldRenderComponent />
+    return <FieldRenderComponent />;
   }
 }
 
 class FieldRenderComponent extends React.Component {
-
   render() {
-    return <div style={{ color: '#fff' }}>
-      Hello from React!
-        </div>;
+    return <div style={{ color: '#fff' }}>Hello from React!</div>;
   }
 }
 
