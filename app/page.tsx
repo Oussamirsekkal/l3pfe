620b6dd56672b89dc457a386c37470942706a0b7
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2024-04-25 10:39:12 +0100

    home page better

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import "./Blockly/App.css"
 import Whyus from "@/app/components/whyus";
+import Main from "@/app/components/main";
 
 export default function Home() {
   return (
-    <main className="flex min-h-auto flex-col items-center justify-between p-24">
-      <Whyus></Whyus>
+    <main className="flex min-h-auto flex-col items-center justify-between p-0">
+        <Main/>
+      <Whyus/>
 
     </main>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,6 +2,7 @@ import react from "react";
 import Signup from "@/app/signup/components/signup";
 
 
+
 export default function Home() {
 
    return(
