2f99694fb3c075831c34d9676cf40b5b7d26618e
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2024-04-30 23:09:41 +0100

    login and signup working with cookies++

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers'
 
-export async function deletecookie(name :string) {
+ async function deletecookie(name :string) {
     cookies().set(name, '', { maxAge: 0, secure: true, httpOnly: true, path: '/' });
 }
 
