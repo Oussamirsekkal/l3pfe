33a5d98db506888e73266d05fe83cbdb0e57af04
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2024-05-04 17:11:20 +0100

    new update

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -61,7 +61,7 @@ const router = useRouter();
                      className="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start">
                     <ul className="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
                         <li className="text-gray-600 md:mr-12 hover:text-blue-600"><a href="/Blockly">Blockly</a></li>
-                        <li className="text-gray-600 md:mr-12 hover:text-blue-600"><a href="#">Features</a></li>
+                        <li className="text-gray-600 md:mr-12 hover:text-blue-600"><a href="/profile">Profile</a></li>
                         <li className="text-gray-600 md:mr-12 hover:text-blue-600"><a href="#">Support</a></li>
                         <li className="text-gray-600 md:mr-12 hover:text-blue-600">
                             {isLoggedIn ? (
